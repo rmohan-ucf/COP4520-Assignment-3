35798eb9654f73e194c8b44a146e68246a8e180e
Rahul Mohan <143127250+rmohan-ucf@users.noreply.github.com>
2024-04-05 23:41:51 -0400

    Initial commit

---
+++ Birthday.java
@@ -0,0 +1,207 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Birthday {
+  public static final int GIFT_COUNT = 500_000;
+  public static final int SERVANT_COUNT = 8;
+
+  public static AtomicInteger counter = new AtomicInteger();// used to pick random presents
+  public static List<Present> bag = new ArrayList<>();
+  public static PresentChain chain = new PresentChain();
+
+  public static Set<Integer> thanked = new HashSet<>();
+
+  // Servant thread
+  public static class Servant extends Thread {
+    public void run() {
+      Random rand = new Random();
+      boolean flip = true;
+
+      while (thanked.size() < GIFT_COUNT) {
+        if (rand.nextInt(50) == 0) {
+          int s = chain.size.get();
+          if (s > 0) {
+            chain.has(rand.nextInt(s));
+          }
+        } else if (flip) {
+          if (bag.size() > 0) {
+            Present x = bag.remove(0);
+            chain.add(x);
+          }
+          flip = !flip;
+        }
+        // !flip
+        else {
+          if (chain.size.get() > 0) {
+            Present x = chain.pop(chain.head.next);
+            thanked.add(x.id);
+          }
+          flip = !flip;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    long startTime = System.nanoTime();
+
+    for (int i = 1; i <= GIFT_COUNT; i++) {
+      bag.add(new Present(i));
+    }
+
+    Collections.shuffle(bag);
+
+    Servant[] servants = new Servant[SERVANT_COUNT];
+    for (int i = 0; i < SERVANT_COUNT; i++) {
+      servants[i] = new Servant();
+      servants[i].start();
+      try {
+        servants[i].join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    long endtime = System.nanoTime();
+
+    System.out.println("Servants finished their tasks...");
+    System.out.println("Size of thank you note stack: " + thanked.size());
+    System.out.println("Presents left on chain: " + chain.size.get());
+    System.out.println("Total execution time " + (endtime - startTime) / 1_000_000_000.0 + " seconds");
+  }
+}
+
+// Class representing a present, doubles as a linked-list node
+class Present implements Comparable<Present> {
+  public int id;
+  public Present next;
+  public boolean marked;
+
+  Lock lock;
+
+  public Present(int id) {
+    this.id = id;
+    this.next = null;
+    this.marked = false;
+    this.lock = new ReentrantLock();
+  }
+
+  void lock() {
+    lock.lock();
+  }
+
+  void unlock() {
+    lock.unlock();
+  }
+
+  @Override
+  public int compareTo(Present other) {
+    return this.id - other.id;
+  }
+}
+
+// Class representing a present linked list
+class PresentChain {
+  Present head;
+  AtomicInteger size;
+
+  public PresentChain() {
+    head = new Present(Integer.MIN_VALUE);
+    head.next = new Present(Integer.MAX_VALUE);
+
+    size = new AtomicInteger(0);
+  }
+
+  public void add(Present x) {
+    while (true) {
+      // Find add position
+      Present prev = head;
+      Present cur = head.next;
+
+      while (cur.id < x.id) {
+        prev = cur;
+        cur = cur.next;
+      }
+
+      prev.lock();
+      try {
+        cur.lock();
+        try {
+          // Ensure nothing has changed between the time the current
+          // node was found and the nodes were locked
+          if (!prev.marked && !cur.marked && prev.next == cur) {
+            // Prevent present from being added to chain twice
+            if (cur.id != x.id) {
+              prev.next = x;
+              x.next = cur;
+
+              size.getAndIncrement();
+
+              return; // Exit loop
+            }
+          }
+        } finally {
+          cur.unlock();
+        }
+      } finally {
+        prev.unlock();
+      }
+    }
+  }
+
+  public Present pop(Present x) {
+    while (true) {
+      // Find pop position
+      Present prev = this.head;
+      Present cur = head.next;
+      while (cur.id < x.id) {
+        prev = cur;
+        cur = cur.next;
+      }
+
+      prev.lock();
+      try {
+        cur.lock();
+        try {
+          // Ensure nothing has changed between the time the current
+          // node was found and the nodes were locked
+          if (!prev.marked && !cur.marked && prev.next == cur) {
+            // Node doesn't exist
+            if (cur.id != x.id) {
+              return null;
+            }
+            // Remove from chain and connect predecessor and successor
+            else {
+              cur.marked = true;
+              prev.next = cur.next;
+
+              size.getAndDecrement();
+
+              return cur;
+            }
+          }
+        } finally {
+          cur.unlock();
+        }
+      } finally {
+        prev.unlock();
+      }
+    }
+  }
+
+  public boolean has(int id) {
+    Present curr = head;
+    while (curr.id < id) {
+      curr = curr.next;
+    }
+    return curr.id == id && !curr.marked;
+  }
+}
---
+++ Temperature.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+public class Temperature {
+    public final static int NUM_THREADS = 8;
+    public final static int PAUSE_TIME = 10;
+
+    public static ArrayList<Sensor> sensors;
+    public static List<ArrayList<Integer>> memory;
+
+    public static class Sensor extends Thread {
+
+        public final static int MAX_TEMP = 70;
+        public final static int MIN_TEMP = -100;
+
+        public boolean hasRead;
+        private int id;
+        private Random rand;
+
+        Sensor(int id) {
+            this.id = id;
+            this.hasRead = false;
+            this.rand = new Random();
+        }
+
+        @Override
+        public void run() {
+            hasRead = true;
+            int sensorValue = rand.nextInt(MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP;
+            memory.get(id).add(sensorValue);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Beginning temperature simulation");
+
+        memory = Collections.synchronizedList(new ArrayList<ArrayList<Integer>>(NUM_THREADS));
+
+        sensors = new ArrayList<>();
+        for (int i = 0; i < NUM_THREADS; i++) {
+            sensors.add(new Sensor(i));
+            memory.add(new ArrayList<Integer>());
+        }
+
+        while (true) {
+            for (Sensor s : sensors) {
+
+                if (!s.hasRead) {
+                    s.start();
+                }
+                else {
+                    s.run();
+                }
+            }
+
+            for (Sensor s : sensors) {
+                try {
+                    s.join();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Print output
+            System.out.printf("Data for minute %-2d:", memory.get(0).size());
+            for (ArrayList<Integer> list : memory) {
+                System.out.printf("   %-4d", list.get((list.size() - 1)));
+            }
+            System.out.println();
+
+            if (memory.get(0).size() >= 60) {
+                output();
+                return;
+            }
+
+            // Sleep to simulate
+            try {
+                Thread.sleep(PAUSE_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void output() {
+
+        List<Integer> sortedReadings = new ArrayList<Integer>();
+        for (List<Integer> l : memory) {
+            sortedReadings.addAll(l);
+        }
+        Collections.sort(sortedReadings);
+
+        int[] min = new int[60];
+        int[] max = new int[60];
+
+        int a = -1, b = -1;
+        int maxDiff = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 60; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < NUM_THREADS; j++) {
+                temp.add(memory.get(j).get(i));
+            }
+
+            min[i] = Collections.min(temp);
+            max[i] = Collections.max(temp);
+
+            if (i >= 10) {
+                if ((max[i] - min[i - 10]) > maxDiff) {
+                    a = i;
+                    b = i - 10;
+                    maxDiff = Math.abs(max[i] - min[i - 10]);
+                }
+
+                if ((max[i - 10] - min[i]) > maxDiff) {
+                    a = i - 10;
+                    b = i;
+                    maxDiff = Math.abs(max[i - 10] - min[i]);
+                }
+            }
+        }
+
+        System.out.println("Temperature Report");
+        System.out.println("--------------------------------");
+        System.out.println("Lowest recorded temperatures:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Integer.toString(sortedReadings.get(i)));
+
+        }
+        System.out.println("Top 5 highest temperatures:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Integer.toString(sortedReadings.get(sortedReadings.size() - 1 - i)));
+
+        }
+        System.out.println("10-minute interval with largest temperature difference:");
+        System.out.println(
+                "Minute " + (Math.min(a, b) + 1) + " to minute " + (Math.max(a, b) + 1) + " with difference of "
+                        + maxDiff + " (" + min[b] + " to " + max[a] + ")");
+        System.out.println();
+    }
+}
